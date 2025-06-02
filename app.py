463bf99184ca830ab471807ee67194293d3f9f72
AdrianSykes <sykesade@googlemail.com>
2025-06-02 15:56:33 +0100

    Update app.py

--- app.py
+++ app.py
@@ -89,6 +89,6 @@ def success():
     return render_template("success.html")
 
 if __name__ == "__main__":
-    port = int(os.getenv("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=int(os.getenv("PORT", 5000)), debug=True)
+
 
